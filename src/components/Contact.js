9225afa8391e78e2e7ff402b87749ae88089e9d9
Logan Davis <Ldavis96@live.com>
2022-04-19 18:59:22 -0400

    deploy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -41,28 +41,8 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=4507+Pepper+Berry+Ln&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/embed/v1/place?q=Columbus,+OH&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
-              </h2>
-              <p className="mt-1">4507 Pepper Berry Ln, Hilliard, OH 43026</p>
-            </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                EMAIL
-              </h2>
-              <a className="text-indigo-400 leading">
-                logandavisjobs@outlook.com
-              </a>
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                PHONE NUMBER
-              </h2>
-              <p className="leading-relaxed">614-806-6934</p>
-            </div>
-          </div>
         </div>
         <form
           netlify
