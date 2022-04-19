26f0612557b8803cc7c99b3269158188de11f368
Logan Davis <Ldavis96@live.com>
2022-04-19 11:23:57 -0400

    seed data for project cards

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "The Forum",
+    subtitle: "Full Stack",
+    description:
+      "Full-Stack collaborative project in which I was responsible for the Express.js server setup and connecting the SQL database to the front-end.",
+    image: "https://i.imgur.com/FGD6tzW.jpg",
+    link: "https://chad-the-forum.herokuapp.com/",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://www.smashbros.com/assets_v2/img/fighter/link/main.png",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://www.smashbros.com/assets_v2/img/fighter/link/main.png",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://www.smashbros.com/assets_v2/img/fighter/link/main.png",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://www.smashbros.com/assets_v2/img/fighter/link/main.png",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://www.smashbros.com/assets_v2/img/fighter/link/main.png",
+    link: "https://jsbootcamp.com",
+  },
+];
