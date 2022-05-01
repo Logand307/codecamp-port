c6514d0a538857870450d896da67e6e852faefb4
Logan Davis <Ldavis96@live.com>
2022-05-01 09:48:17 -0400

    deploy, added project

--- src/data.js
+++ src/data.js
@@ -10,14 +10,16 @@ export const projects = [
     github: "https://github.com/cmrotruck/the-forum",
   },
   {
-    title: "Pizza Hunt",
-    subtitle: "HTML5, CSS3, Node.js, Express.js, MongoDB, Mongoose, IndexedDB",
+    title: "Shop-Shop",
+    subtitle:
+      "MongoDB, Mongoose, Express.js, Node.js, React.js, Global State, Stripe Payment Platform",
     description:
-      "Application where users are able to collaborate with one another and update the database while offline with data persistence.",
-    image: "https://i.imgur.com/Mhpt5RU.jpg",
-    link: "https://shielded-basin-55815.herokuapp.com/",
-    github: "https://github.com/Logand307/pizza-hunt",
+      "Shopify clone where users can simulate purchasing experience after being authenticated.",
+    image: "https://i.imgur.com/ofndBSp.jpg",
+    link: "https://fathomless-caverns-70000.herokuapp.com/",
+    github: "https://github.com/Logand307/shop-shop",
   },
+
   {
     title: "React Photographer Portfolio",
     subtitle: "React.js, Node.js, HTML5, CSS3",
@@ -27,15 +29,15 @@ export const projects = [
     link: "https://stormy-beyond-65957.herokuapp.com/",
     github: "https://github.com/Logand307/photo-port",
   },
-  // {
-  //   title: "Email Matching Tutorial",
-  //   subtitle: "Javascript, Regex",
-  //   description:
-  //     "Thorough explanation of what regular expressions are and how they can be used to validate an email address.",
-  //   image: "https://i.imgur.com/xWmIt55.jpg",
-  //   link: "https://gist.github.com/Logand307/eb1a1ed240f23ea46be28f235114263a",
-  //   github: "https://github.com/Logand307/REGEX_Tutorial",
-  // },
+  {
+    title: "Email Matching Tutorial",
+    subtitle: "Javascript, Regex",
+    description:
+      "Thorough explanation of what regular expressions are and how they can be used to validate an email address.",
+    image: "https://i.imgur.com/xWmIt55.jpg",
+    link: "https://gist.github.com/Logand307/eb1a1ed240f23ea46be28f235114263a",
+    github: "https://github.com/Logand307/REGEX_Tutorial",
+  },
   {
     title: "Social-Network-API",
     subtitle: "Node.js, Express.js, MongoDB, Mongoose, RESTful APIs",
@@ -54,6 +56,15 @@ export const projects = [
   //   link: "https://bootcamp-zoo.herokuapp.com/",
   //   github: "https://github.com/cmrotruck/bootcamp-zoo",
   // },
+  {
+    title: "Pizza Hunt",
+    subtitle: "HTML5, CSS3, Node.js, Express.js, MongoDB, Mongoose, IndexedDB",
+    description:
+      "Application where users are able to collaborate with one another and update the database while offline with data persistence.",
+    image: "https://i.imgur.com/Mhpt5RU.jpg",
+    link: "https://shielded-basin-55815.herokuapp.com/",
+    github: "https://github.com/Logand307/pizza-hunt",
+  },
 ];
 
 export const skills = [
